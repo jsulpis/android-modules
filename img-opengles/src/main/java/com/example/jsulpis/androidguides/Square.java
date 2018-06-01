b47f8b7e8f9502811f601b2165f7919d7bbaae02
Julien Sulpis <julien.sulpis@gmail.com>
2018-06-01 17:59:29 +0200

    img-opengles: draw a Triangle or a Square on the screen

--- img-opengles/src/main/java/com/example/jsulpis/androidguides/Square.java
+++ img-opengles/src/main/java/com/example/jsulpis/androidguides/Square.java
@@ -1,14 +1,19 @@
 package com.example.jsulpis.androidguides;
 
+import android.opengl.GLES20;
+
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
 import java.nio.ShortBuffer;
 
+/**
+ * A square shape with all functions needed to display it on a screen
+ */
 public class Square {
 
     private FloatBuffer vertexBuffer;
-    private ShortBuffer drawListBuffer;
+    private ShortBuffer drawListBuffer; // The order of the vertices to draw
 
     // number of coordinates per vertex in this array
     static final int COORDS_PER_VERTEX = 3;
@@ -19,6 +24,34 @@ public class Square {
             0.5f,  0.5f, 0.0f }; // top right
 
     private short drawOrder[] = { 0, 1, 2, 0, 2, 3 }; // order to draw vertices
+
+    private final int vertexCount = squareCoords.length / COORDS_PER_VERTEX;
+    private final int vertexStride = COORDS_PER_VERTEX * 4; // 4 bytes per vertex
+
+    // Set color with red, green, blue and alpha (opacity) values
+    float color[] = { 0.63671875f, 0.76953125f, 0.22265625f, 1.0f };
+
+    // OpenGL ES graphics code for rendering the vertices of a shape
+    private final String vertexShaderCode =
+            "attribute vec4 vPosition;" +
+                    "void main() {" +
+                    "  gl_Position = vPosition;" +
+                    "}";
+
+    // OpenGL ES code for rendering the face of a shape with colors or textures
+    private final String fragmentShaderCode =
+            "precision mediump float;" +
+                    "uniform vec4 vColor;" +
+                    "void main() {" +
+                    "  gl_FragColor = vColor;" +
+                    "}";
+
+    // OpenGL ES object that contains the shaders you want to use for drawing one or more shapes
+    private final int mProgram;
+
+    private int mPositionHandle;
+    private int mColorHandle;
+
 
     public Square() {
         // initialize vertex byte buffer for shape coordinates
@@ -38,5 +71,51 @@ public class Square {
         drawListBuffer = dlb.asShortBuffer();
         drawListBuffer.put(drawOrder);
         drawListBuffer.position(0);
+
+        int vertexShader = MyGLRenderer.loadShader(GLES20.GL_VERTEX_SHADER, vertexShaderCode);
+        int fragmentShader = MyGLRenderer.loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentShaderCode);
+
+        // Create empty OpenGL ES Program
+        mProgram = GLES20.glCreateProgram();
+
+        // Add the vertex shader to program
+        GLES20.glAttachShader(mProgram, vertexShader);
+
+        // Add the fragment shader to program
+        GLES20.glAttachShader(mProgram, fragmentShader);
+
+        // Create OpenGL ES program executables
+        GLES20.glLinkProgram(mProgram);
+    }
+
+    /**
+     * Calling this function will draw the shape on the screen.
+     */
+    public void draw() {
+        // Add program to OpenGL ES environment
+        GLES20.glUseProgram(mProgram);
+
+        // get handle to vertex shader's vPosition member
+        mPositionHandle = GLES20.glGetAttribLocation(mProgram, "vPosition");
+
+        // Enable a handle to the square vertices
+        GLES20.glEnableVertexAttribArray(mPositionHandle);
+
+        // Prepare the square coordinate data
+        GLES20.glVertexAttribPointer(mPositionHandle, COORDS_PER_VERTEX,
+                GLES20.GL_FLOAT, false,
+                vertexStride, vertexBuffer);
+
+        // get handle to fragment shader's vColor member
+        mColorHandle = GLES20.glGetUniformLocation(mProgram, "vColor");
+
+        // Set color for drawing the square
+        GLES20.glUniform4fv(mColorHandle, 1, color, 0);
+
+        // Draw the square
+        GLES20.glDrawElements(GLES20.GL_TRIANGLES, drawOrder.length, GLES20.GL_UNSIGNED_SHORT, drawListBuffer);
+
+        // Disable vertex array
+        GLES20.glDisableVertexAttribArray(mPositionHandle);
     }
 }
--- img-opengles/src/main/java/com/example/jsulpis/androidguides/Triangle.java
+++ img-opengles/src/main/java/com/example/jsulpis/androidguides/Triangle.java
@@ -1,23 +1,53 @@
 package com.example.jsulpis.androidguides;
 
+import android.opengl.GLES20;
+
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
 
+/**
+ * A Triangle shape with all functions needed to display it on a screen
+ */
 public class Triangle {
 
     private FloatBuffer vertexBuffer;
 
     // number of coordinates per vertex in this array
     static final int COORDS_PER_VERTEX = 3;
-    static float triangleCoords[] = {   // in counterclockwise order:
+    private static float triangleCoords[] = {   // in counterclockwise order:
             0.0f,  0.622008459f, 0.0f, // top
             -0.5f, -0.311004243f, 0.0f, // bottom left
             0.5f, -0.311004243f, 0.0f  // bottom right
     };
 
+    private final int vertexCount = triangleCoords.length / COORDS_PER_VERTEX;
+    private final int vertexStride = COORDS_PER_VERTEX * 4; // 4 bytes per vertex
+
     // Set color with red, green, blue and alpha (opacity) values
     float color[] = { 0.63671875f, 0.76953125f, 0.22265625f, 1.0f };
+
+    // OpenGL ES graphics code for rendering the vertices of a shape
+    private final String vertexShaderCode =
+            "attribute vec4 vPosition;" +
+                    "void main() {" +
+                    "  gl_Position = vPosition;" +
+                    "}";
+
+    // OpenGL ES code for rendering the face of a shape with colors or textures.
+    private final String fragmentShaderCode =
+            "precision mediump float;" +
+                    "uniform vec4 vColor;" +
+                    "void main() {" +
+                    "  gl_FragColor = vColor;" +
+                    "}";
+
+    // OpenGL ES object that contains the shaders you want to use for drawing one or more shapes
+    private final int mProgram;
+
+    private int mPositionHandle;
+    private int mColorHandle;
+
 
     public Triangle() {
         // initialize vertex byte buffer for shape coordinates
@@ -33,5 +63,51 @@ public class Triangle {
         vertexBuffer.put(triangleCoords);
         // set the buffer to read the first coordinate
         vertexBuffer.position(0);
+
+        int vertexShader = MyGLRenderer.loadShader(GLES20.GL_VERTEX_SHADER, vertexShaderCode);
+        int fragmentShader = MyGLRenderer.loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentShaderCode);
+
+        // Create empty OpenGL ES Program
+        mProgram = GLES20.glCreateProgram();
+
+        // Add the vertex shader to program
+        GLES20.glAttachShader(mProgram, vertexShader);
+
+        // Add the fragment shader to program
+        GLES20.glAttachShader(mProgram, fragmentShader);
+
+        // Create OpenGL ES program executables
+        GLES20.glLinkProgram(mProgram);
+    }
+
+    /**
+     * Calling this function will draw the shape on the screen.
+     */
+    public void draw() {
+        // Add program to OpenGL ES environment
+        GLES20.glUseProgram(mProgram);
+
+        // get handle to vertex shader's vPosition member
+        mPositionHandle = GLES20.glGetAttribLocation(mProgram, "vPosition");
+
+        // Enable a handle to the triangle vertices
+        GLES20.glEnableVertexAttribArray(mPositionHandle);
+
+        // Prepare the triangle coordinate data
+        GLES20.glVertexAttribPointer(mPositionHandle, COORDS_PER_VERTEX,
+                GLES20.GL_FLOAT, false,
+                vertexStride, vertexBuffer);
+
+        // get handle to fragment shader's vColor member
+        mColorHandle = GLES20.glGetUniformLocation(mProgram, "vColor");
+
+        // Set color for drawing the triangle
+        GLES20.glUniform4fv(mColorHandle, 1, color, 0);
+
+        // Draw the triangle
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertexCount);
+
+        // Disable vertex array
+        GLES20.glDisableVertexAttribArray(mPositionHandle);
     }
 }
