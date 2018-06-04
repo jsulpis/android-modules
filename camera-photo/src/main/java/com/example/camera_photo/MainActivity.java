dbaa09fad372fe4c5f6b56d9d309f7eaf73247c9
Julien Sulpis <julien.sulpis@gmail.com>
2018-06-04 09:46:21 +0200

    camera-photo: take a photo by an external app and get the thumbnail back

---
+++ camera-photo/src/main/java/com/example/camera_photo/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.camera_photo;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView mImageView;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mImageView = findViewById(R.id.picture);
+        dispatchTakePictureIntent();
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Make sure that at least one app can handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // If so, launch the Intent and ask for the result
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageView.setImageBitmap(imageBitmap);
+        }
+    }
+}
